4e8ba87bb62a0435a4866958150a9b631cd9cdcf
Gavin Ridley <gridley@vols.utk.edu>
2017-08-04 16:21:57 -0500

    Added script that plots power over the flow loss case

--- flowLossPlot.py
+++ flowLossPlot.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-# makes a plot for the flow loss plot
+# makes a plot for the flow loss case
 # note: you need to comment out the header on the csv file
 import matplotlib.pyplot as pl
 from matplotlib import rc
 import numpy as np
 
 # directory with output data:
-outdir = ('/home/gavin/projects/moltres/problems/'
-          'publication_level_cases/twod_control_rod_yank/')
+outdir = ('/home/gav/projects/moltres/problems/'
+          'LOFA/')
 
 data = np.loadtxt(outdir+'out.csv', delimiter=',')
 
@@ -25,3 +25,20 @@ colname2col = dict(zip(colnames, range(len(colnames))))
 rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'], 'size': 20})
 rc('text', usetex=True)
 pl.style.use('ggplot')  # mwahaha, now people think i know R
+
+fig, ax = pl.subplots( figsize=(6,4))
+ax.set_title('Power vs. salt flow velocity in core')
+ax.set_xlabel('Time (s)')
+ax2 = ax.twinx()
+ax.set_ylabel('Salt flow velocity (cm/s)', color='b')
+ax2.set_ylabel('Average nuclear power (W/cm$^3$)', color='r')
+def flowRate(t):
+    """ gives fixed flow rate as a function of time
+    notice that it depends on pump coastdown time constant, so
+    be sure to change that if needed. right now, it's set to 5s """
+    tau = 5.0
+    return 21.7 * np.exp(-t/tau)
+ax.plot(data[:,0], data[:,1], 'r-*')
+ax2.plot(data[:,0], flowRate(data[:,0]), 'b-*')
+
+fig.savefig('pumpFail.png')
